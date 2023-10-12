9acd6f8b527db5c0172e9bc95700e36511f01427
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2023-10-12 12:21:56 +0200

    setting up the navbar-app with an express server, using the built-in static express middleware to serve static files

--- 02-express-tutorial/express-server.js
+++ 02-express-tutorial/express-server.js
@@ -1,21 +1,26 @@
-const express = require('express')
-const app = express()
+/*
+====================
+BASIC EXPRESS SERVER
+====================
+*/
+// const express = require('express')
+// const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).send('Home Page')
-})
+// app.get('/', (req, res) => {
+//   res.status(200).send('Home Page')
+// })
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About page')
-})
+// app.get('/about', (req, res) => {
+//   res.status(200).send('About page')
+// })
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource not found</h1>')
-})
+// app.all('*', (req, res) => {
+//   res.status(404).send('<h1>Resource not found</h1>')
+// })
 
-app.listen(5000, () => {
-  console.log('server is up and running')
-})
+// app.listen(5000, () => {
+//   console.log('server is up and running')
+// })
 
 // app.get
 // app.post
@@ -24,3 +29,31 @@ app.listen(5000, () => {
 // app.all
 // app.use
 // app.listen
+
+/*
+=========================
+NAVBAR EXPRESS SERVER APP
+=========================
+*/
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// setup static and middleware
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+// res.sendFile(path.resolve('./navbar-app/index.html'))
+// alternatively, we can just put the html file inside our static assets folder
+// or send the file via SSR (Server-side rendering using template engines)
+// })
+
+app.all('*', (req, res) => {
+  res.status(404).send('Resource not found')
+})
+
+app.listen(5000, () => {
+  console.log('Server is up and running')
+})
