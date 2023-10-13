58c80c7c72d0e0afa0391996fc483f47c5e2a21e
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2023-10-13 21:45:02 +0200

    route parameters and query parameters in express

--- 02-express-tutorial/express-api.js
+++ 02-express-tutorial/express-api.js
@@ -1,10 +1,50 @@
 const express = require('express')
 const app = express()
 
-const { products } = require('./data')
+let { products } = require('./data')
 
 app.get('/', (req, res) => {
-  res.json(products)
+  res.send('<h1> Home page </h1> <a href="/api/products"> products </a>')
+})
+
+app.get('/api/products', (req, res) => {
+  let newProducts = products.map((product) => {
+    let { id, name, image } = product
+    return { id, name, image }
+  })
+  res.json(newProducts)
+})
+
+//ROUTE PARAMETERS (req.params)
+app.get('/api/products/:productID', (req, res) => {
+  let { productID } = req.params
+  let product = products.find((product) => {
+    return product.id === Number(productID)
+  })
+  if (!product) {
+    res.status(404).send('Sorry, that resource does not exist.')
+  }
+  res.json(product)
+})
+
+// QUERY PARAMETERS (req.query) (technically, they are not part of the URL, if they are present however, they can be handled by the server)
+app.get('/api/v1/query', (req, res) => {
+  // create copy of products, so as to not modify the original one
+  let sortedProducts = [...products]
+  let { search, limit } = req.query
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(404).json({ success: true, data: [] })
+  }
+  res.status(200).json(sortedProducts)
 })
 
 app.listen(5000, () => {
