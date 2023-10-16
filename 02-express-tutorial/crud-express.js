4c8d89fe61bb2057817f2e8f907a3f8b869a2f0f
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2023-10-16 21:59:28 +0200

    HTTP POST method (using forms, action="/login" method="POST")

---
+++ 02-express-tutorial/crud-express.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+let { people } = require('./data')
+
+// static assets
+app.use(express.static('./methods-public'))
+
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
+})
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  console.log(req.body)
+  res.status(401).send('Please Provide Credentials')
+})
+
+app.listen(5000, () => {
+  console.log('Server is up and running')
+})
