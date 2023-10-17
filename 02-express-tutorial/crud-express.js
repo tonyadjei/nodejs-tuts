987d82ce72b4ca5c9aec5860f335c7a68ce7e667
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2023-10-17 22:21:43 +0200

    HTTP POST method using JavaScript code, with axios

--- 02-express-tutorial/crud-express.js
+++ 02-express-tutorial/crud-express.js
@@ -9,8 +9,21 @@ app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({ extended: false }))
 
+// parse json
+app.use(express.json())
+
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
+})
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'Please provide name value' })
+  }
+  res.status(201).json({ sucess: true, person: name })
 })
 
 app.post('/login', (req, res) => {
